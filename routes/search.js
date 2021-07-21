6090f132446291ebba2b4e67e47160fcd535b92b
kellraym <ray.al.kell@gmail.com>
2021-07-21 14:16:31 -0600

    add console log to search route, change params

--- routes/search.js
+++ routes/search.js
@@ -3,10 +3,11 @@ var router = express.Router();
 const knex = require('knex')(require('../knexfile')['production']);
 
 router.get('/', function (req, res) {
-  const name = req.params.name
-  knex.select('name')
+  const name = `%${req.params.name}$`
+  console.log(name)
+  knex.select('id', 'name', 'due_date', 'description')
     .from('todos')
-    .where('name', 'like', `%${name}%`)
+    .where('name', 'like', name)
     .then(data => res.status(200).send(data))
     .catch(err => res.status(404).send(err))
 })
