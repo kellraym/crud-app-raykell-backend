a95fb5971a3bc37f758cf79d7ad81fe1be8bbc25
kellraym <ray.al.kell@gmail.com>
2021-07-21 14:43:57 -0600

    add completed selection for basic search

--- routes/search.js
+++ routes/search.js
@@ -5,7 +5,7 @@ const knex = require('knex')(require('../knexfile')['production']);
 router.get('/', function (req, res) {
   const name = `%${req.query.name}%`
   console.log(name)
-  knex.select('id', 'name', 'due_date', 'description')
+  knex.select('id', 'name', 'due_date', 'description', 'complete')
     .from('todos')
     .where('name', 'like', name)
     .then(data => res.status(200).send(data))
